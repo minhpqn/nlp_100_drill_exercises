9d4f04b7b5eac23ba54c5b2bc7b3cf151cb90792
minhpham <nhatminhk47ca@yahoo.com>
2015-09-29 09:09:49 +0900

    2015年 9月29日 火曜日 09時09分49秒 JST version

--- python/chap01/ex09.py
+++ python/chap01/ex09.py
@@ -1,20 +1,38 @@
 # -*- coding: utf-8 -*-
+import random
 
 ### 09. [Typoglycemia](https://en.wikipedia.org/wiki/Typoglycemia)
 # Cho đầu vào là một câu tiếng Anh bao gồm các word ngăn cách nhau bằng ký tự space. Viết chương trình thực hiện việc sau:
 # - Với mỗi word, giữ nguyên ký tự đầu và ký tự cuối, đảo thứ tự một cách ngẫu nhiên các ký tự còn lại của (tất nhiên các word có ít hơn 4 ký tự thì không cần làm gì)
 # - Cho trước một câu tiếng Anh hợp lệ, ví dụ "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind .", chạy chương trình đã viết để đưa ra kết quả.
-
+    
 def typoglycemia(s):
-    # TODO: implement the function
-    news = s
-    return news
+    news = []
+    words = s.split()
+    for w in words:
+        news.append(process(w))
+    return ' '.join(news)
 
+def process(w):
+    if len(w) < 4:
+        return w
+
+    neww = w[0]
+    random_index = range(1,len(w)-1)
+
+    random.shuffle(random_index)
+    for i in random_index:
+        neww += w[i]
+        
+    neww += w[-1]
+    return neww
+    
 def main():
+    random.seed(99999)
     s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 
-    print "Original string: %s" % s
-    print "Typoglycemia string: %s" % typoglycemia(s)
+    print "-- Original string: %s" % s
+    print "-- Typoglycemia string: %s" % typoglycemia(s)
 
 
 if __name__ == '__main__':
