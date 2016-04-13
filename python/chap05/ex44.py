de8c2b7ed56a1bdc2bcc2edcdd594c1ae7d9d7ba
Minh Pham <minhpham@arrowhead.nict.go.jp>
2016-04-13 18:14:44 +0900

    Finished chapter 5

---
+++ python/chap05/ex44.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+""" 44. Visualize cây dependency
+"""
+import pydot
+from collections import defaultdict
+import sys
+from chunk_ex41 import read_chunks
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL)
+
+def draw_dependency_tree(sen):
+    """
+    Create a directed graph
+    For each chunk in the sentence, create a node
+        Append to a list of node
+    For each chunk, create an edge from the chunk to the head
+    Draw the directed graph
+    """
+    graph = pydot.Dot(graph_type='digraph')
+    nodes = []
+    for i,ck in enumerate(sen):
+        lb = '%d- %s' % (i, ck.surf2())
+        node = pydot.Node( lb )
+        nodes.append(node)
+        
+    for i,ck in enumerate(sen):
+        j = ck.dst
+        if j == -1: continue
+        graph.add_edge(pydot.Edge(nodes[i], nodes[j]))
+
+    graph.write_jpeg('dependency_tree.jpg', prog='dot')
+
+if __name__ == '__main__':
+    sentences = read_chunks()
+
+    sen = sentences[7]
+    draw_dependency_tree(sen)
+    
+
--- python/chap05/ex49.py
+++ python/chap05/ex49.py
@@ -18,7 +18,7 @@ def get_path(ch, listid, sen):
     chain.append( ck_repr(ch, ck) )
     for i in range(1, len(listid)):
         chain.append(sen[listid[i]].surf2())
-    return ' -> '.join(chain)    
+    return ' -> '.join(chain)
 
 sentences = read_chunks()
 for sen in sentences:
